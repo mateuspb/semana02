62be0de615e719037af88fb20e653b84e63056f4
mateuspb <mateusperin@gmail.com>
2020-05-19 12:58:05 -0300

    Add files via upload

--- csharp-3/Source/State.cs
+++ csharp-3/Source/State.cs
@@ -9,11 +9,17 @@ namespace Codenation.Challenge
             this.Name = name;
             this.Acronym = acronym;
         }
-
+        public State(string name, string acronym, double extension)
+        {
+            this.Name = name;
+            this.Acronym = acronym;
+            this.Extension = extension;
+        }
         public string Name { get; set; }
 
         public string Acronym { get; set; }
 
+        public double Extension { get; set; }
     }
 
 }
